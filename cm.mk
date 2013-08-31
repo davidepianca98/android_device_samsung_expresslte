51c336a1f20cb7b8a9b16bdef87d41fba7851f72
KINGbabasula <kingbabasula@gmail.com>
2013-08-31 09:29:36 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=expresspuc TARGET_DEVICE=express BUILD_FINGERPRINT="samsung/expresspuc/express:4.1.2/JZO54K/I437PUCAME2:user/release-keys" PRIVATE_BUILD_DESC="expresspuc-user 4.1.2 JZO54K I437PUCAME2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=expresslte TARGET_DEVICE=express BUILD_FINGERPRINT="samsung/expresslte/express:4.1.2/JZO54K/I8730DXAMC3:user/release-keys" PRIVATE_BUILD_DESC="expressLTE-user 4.1.2 JZO54K I8730DXAMC3 release-keys"
 
 PRODUCT_NAME := cm_express
 PRODUCT_DEVICE := express
