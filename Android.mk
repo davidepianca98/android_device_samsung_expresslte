31a4a803a50df44563e6e677cb358b683d1d0284
KINGbabasula <kingbabasula@gmail.com>
2013-10-08 15:01:45 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8930)
 ifneq ($(filter express,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
